4d0ccbad2bbe4d0f59326cb5273e0810dd3aec9b
Mamon <cszetsu@gmail.com>
2018-01-19 21:35:29 +0800

    ~add patient

--- H-M-S-2/controllers/nurseRoute.js
+++ H-M-S-2/controllers/nurseRoute.js
@@ -17,10 +17,15 @@ module.exports = function(app){
   });
 
   app.post('/nurse', function(req, res){
+    var data = req.body;
     if(req.session.email){
       if(req.session.sino == 'nurse'){
-        connection.query("SELECT * FROM nurse where account_id = '"+req.session.Aid+"';", function(err, rows, fields){
-          res.render('nurse', {data: rows[0].name});
+        connection.query("INSERT INTO patient (name, address, sex, birth_date, age, blood_type) VALUES ("+JSON.stringify(data.name)+","+JSON.stringify(data.address)+","+data.sex+","+data.birth+","+data.age+","+JSON.stringify(data.blood)+");", function(err, rows, fields){
+          if(err){
+            console.log(err);
+          } else {
+            res.render('nurse', {data: "hoy"});
+          }
         });
       } else {
         res.redirect(req.session.sino);
@@ -31,9 +36,3 @@ module.exports = function(app){
   });
 
 }
-
-
-// UPDATE tobeupdated
-
-// INNER JOIN original USING (account_id)
-// SET tobeupdated.id = original.id
