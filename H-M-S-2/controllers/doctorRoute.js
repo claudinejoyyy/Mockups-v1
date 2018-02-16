0d52ada70a6cb2b72849b04ec0b613c1c0caf998
claudinejoyyy <2155912@slu.edu.ph>
2018-02-16 17:11:10 +0800

    ~NURSE(INITIAL ASSESSMENT2)

--- H-M-S-2/controllers/doctorRoute.js
+++ H-M-S-2/controllers/doctorRoute.js
@@ -44,7 +44,7 @@ var user, Aid;
                     var parseTime      = splitDateNTime[1] + ':00';
                     var parseDateNTime = parseDate+' '+parseTime;
                     var addAppointment = 'INSERT into appointment (doctor_id, patient_id, appointment_timestamp, remarks) VALUES ('+Aid+', '+data.appointmentPatientID+', "'+parseDateNTime+'", "'+data.appointmentRemarks+'");';
-                    db.query(addAppointment, function(err){
+                    db.query(addAppointment + 'INSERT into activity_logs(account_id, time, type, remarks) VALUES ('+Aid+',"'+currentTime+'", "appointment", "Set Appointment with '+data.appointmentPatientName+' on '+parseDateNTime+'");', function(err){
                       if (err) {
                         console.log(err);
                       }
