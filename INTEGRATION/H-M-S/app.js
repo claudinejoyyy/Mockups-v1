e19b385df92bb9801bd1ed655a80487922ac608f
Mamon <cszetsu@gmail.com>
2018-02-17 20:40:39 +0800

    ~LEGIT(Starting Point)

---
+++ INTEGRATION/H-M-S/app.js
@@ -0,0 +1,68 @@
+var db = require("./database/db.js");
+var login = require('./controllers/routes');
+var nurse = require('./controllers/nurseRoute');
+var doctor = require('./controllers/doctorRoute');
+var admin = require('./controllers/adminRoute');
+var pharmacist = require('./controllers/pharmacistRoute');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var express = require('express');
+var moment = require('moment');
+var helmet = require('helmet');
+var app = express();
+
+app.locals.moment = require('moment');
+app.set('view engine', 'ejs');
+app.use(helmet.noCache());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.static('./public'));
+app.use(session({secret: 'shhhhh', cookie: { maxAge: 3600000 }}));
+
+
+
+//PARA sa DASHBOARD of all MODULES!!!
+var name   = "SELECT name FROM user_accounts where account_id = ?;";
+var counts = "SELECT (SELECT count(patient_id) er FROM admit WHERE department = 'OPD') as OPD, "
+            +"(SELECT count(patient_id) er FROM admit WHERE department = 'ward') as WARD,"
+            +"(SELECT count(name) pCount FROM patient) as totalRegisteredPatients,"
+            +"(SELECT count(patient_id) admitted FROM admit) as currentlyAdmitted;";
+var chart  = "SELECT  (SELECT count(patient_id) from patient where patient_type = 'cadet') as cadet,"
+            +"(SELECT count(patient_id) from patient where patient_type = 'military officer') as military_officer,"
+            +"(SELECT count(patient_id) from patient where patient_type = 'military dependent') as military_dependent,"
+            +"(SELECT count(patient_id) from patient where patient_type = 'civilian') as civilian,"
+            +"(SELECT count(patient_id) from patient where patient_type = 'authorized civilian') as authorized_civilian;";
+//COUNT
+var whoOPD               = "SELECT p.name FROM admit INNER JOIN patient p USING(patient_id) WHERE department = 'opd';";
+var whoWARD              = "SELECT p.name FROM admit INNER JOIN patient p USING(patient_id) WHERE department = 'ward';";
+var whoCurrentlyAdmitted = "SELECT p.name, a.patient_id FROM admit a INNER JOIN patient p USING(patient_id);";
+// GRAPH
+var currentTime = moment().format('YYYY');
+var monthlyPatientCount  = 'SELECT (SELECT count(logs_id) from activity_logs WHERE time BETWEEN "'+currentTime+'-01-01 00:00:00" and "'+currentTime+'-02-01 00:00:00" and type = "add") as JAN,'
+                          +'(SELECT count(logs_id) from activity_logs WHERE time BETWEEN "'+currentTime+'-02-01 00:00:00" and "'+currentTime+'-03-01 00:00:00" and type = "add") as FEB,'
+                          +'(SELECT count(logs_id) from activity_logs WHERE time BETWEEN "'+currentTime+'-03-01 00:00:00" and "'+currentTime+'-04-01 00:00:00" and type = "add") as MARCH,'
+                          +'(SELECT count(logs_id) from activity_logs WHERE time BETWEEN "'+currentTime+'-04-01 00:00:00" and "'+currentTime+'-05-01 00:00:00" and type = "add") as APRIL,'
+                          +'(SELECT count(logs_id) from activity_logs WHERE time BETWEEN "'+currentTime+'-05-01 00:00:00" and "'+currentTime+'-06-01 00:00:00" and type = "add") as MAY,'
+                          +'(SELECT count(logs_id) from activity_logs WHERE time BETWEEN "'+currentTime+'-06-01 00:00:00" and "'+currentTime+'-07-01 00:00:00" and type = "add") as JUNE,'
+                          +'(SELECT count(logs_id) from activity_logs WHERE time BETWEEN "'+currentTime+'-07-01 00:00:00" and "'+currentTime+'-08-01 00:00:00" and type = "add") as JULY,'
+                          +'(SELECT count(logs_id) from activity_logs WHERE time BETWEEN "'+currentTime+'-08-01 00:00:00" and "'+currentTime+'-09-01 00:00:00" and type = "add") as AUGUST,'
+                          +'(SELECT count(logs_id) from activity_logs WHERE time BETWEEN "'+currentTime+'-09-01 00:00:00" and "'+currentTime+'-010-01 00:00:00" and type = "add") as SEPTEMBER,'
+                          +'(SELECT count(logs_id) from activity_logs WHERE time BETWEEN "'+currentTime+'-010-01 00:00:00" and "'+currentTime+'-011-01 00:00:00" and type = "add") as OCTOBER,'
+                          +'(SELECT count(logs_id) from activity_logs WHERE time BETWEEN "'+currentTime+'-011-01 00:00:00" and "'+currentTime+'-012-01 00:00:00" and type = "add") as NOVEMBER,'
+                          +'(SELECT count(logs_id) from activity_logs WHERE time BETWEEN "'+currentTime+'-012-01 00:00:00" and "'+currentTime+'-001-01 00:00:00" and type = "add") as DECEMBER;';
+//TO DO LIST
+//OTHERS
+var patientList          = "SELECT * from patient;";
+var doctorList           = "SELECT * FROM user_accounts WHERE account_type = 'doctor';";
+var availableBeds        = "SELECT b.bed_id, p.patient_type, p.name, b.status, b.allotment_timestamp from bed b LEFT JOIN patient p USING(patient_id) where b.status = 'Unoccupied';";
+
+var currentTime = moment().format('YYYY-MM-DD HH:mm:ss');
+login (app,db,currentTime);
+nurse (app,db,currentTime,name,counts,chart,whoCurrentlyAdmitted,whoOPD,whoWARD,monthlyPatientCount,patientList,doctorList);
+doctor(app,db,currentTime,name,counts,chart,whoCurrentlyAdmitted,whoOPD,whoWARD,monthlyPatientCount,patientList,availableBeds);
+admin (app,db,currentTime,name,counts,chart,whoCurrentlyAdmitted,whoOPD,whoWARD,monthlyPatientCount,patientList,availableBeds);
+pharmacist(app,db,currentTime);
+
+//port
+app.listen(3001);
+console.log('hello !!');
---
+++ INTEGRATION/H-M-S/controllers/doctorRoute.js
@@ -0,0 +1,84 @@
+module.exports = function(app,db,currentTime,name,counts,chart,whoCurrentlyAdmitted,whoOPD,whoWARD,monthlyPatientCount,patientList,availableBeds){
+var user, Aid, availableBedss;
+
+  app.get('/doctor/dashboard', function(req, res){
+    if(req.session.email && req.session.sino == 'doctor'){
+      Aid = req.session.Aid;
+      if(req.session.sino == 'doctor'){
+        var todoList    = "SELECT * from todo_list where account_id = "+Aid+";";
+        db.query(name + counts + chart + whoCurrentlyAdmitted + whoOPD + whoWARD + patientList + availableBeds + monthlyPatientCount + todoList, Aid, function(err, rows, fields){
+          if (err) {
+            console.log(err);
+          }
+          user = rows[0];
+          availableBedss = rows[7];
+          res.render('doctor/dashboard', {counts:rows[1], chart:rows[2], whoCurrentlyAdmitted:rows[3], whoOPD:rows[4], whoWARD:rows[5], patientList:rows[6],
+                                         availableBeds:rows[7], monthlyPatientCount:rows[8], todoList:rows[9], username: user});
+       });
+      } else {
+        res.redirect(req.session.sino + '/dashboard');
+      }
+    } else {
+      res.redirect('../login');
+    }
+  });
+
+  app.post('/doctor/dashboard', function(req, res){
+    var data = req.body;
+    if(req.session.email && req.session.sino == 'doctor'){
+        if (req.session.sino == 'doctor') {
+              if(data.sub == 'addTodo') {
+                    var splitDateNTime = data.dateNtime.split('T');
+                    var parseDate      = splitDateNTime[0];
+                    var parseTime      = splitDateNTime[1] + ':00';
+                    var parseDateNTime = parseDate+' '+parseTime;
+
+                    var addTodo  = 'INSERT into todo_list (description, date, account_id) VALUES("'+data.description+'","'+parseDateNTime+'",'+Aid+');';
+                    db.query(addTodo + 'INSERT into activity_logs(account_id, time, type, remarks) VALUES ('+Aid+',"'+currentTime+'", "todo", "Added to To Do List the following: '+data.description+'");', function(err){
+                      if (err) {
+                        console.log(err);
+                      }
+                    });
+                    res.redirect(req.get('referer'));
+              } else if(data.sub == 'appointment') {
+                    var splitDateNTime = data.dateNtime.split('T');
+                    var parseDate      = splitDateNTime[0];
+                    var parseTime      = splitDateNTime[1] + ':00';
+                    var parseDateNTime = parseDate+' '+parseTime;
+                    var addAppointment = 'INSERT into appointment (doctor_id, patient_id, appointment_timestamp, remarks) VALUES ('+Aid+', '+data.appointmentPatientID+', "'+parseDateNTime+'", "'+data.appointmentRemarks+'");';
+                    db.query(addAppointment + 'INSERT into activity_logs(account_id, time, type, remarks) VALUES ('+Aid+',"'+currentTime+'", "appointment", "Set Appointment with '+data.appointmentPatientName+' on '+parseDateNTime+'");', function(err){
+                      if (err) {
+                        console.log(err);
+                      }
+                    });
+                    res.redirect(req.get('referer'));
+              }
+        } else {
+          res.redirect(req.session.sino + '/dashboard');
+        }
+    } else {
+      res.redirect('../login');
+    }
+  });
+
+  app.get('/doctor/outpatientManagement', function(req, res){
+    if(req.session.email && req.session.sino == 'doctor'){
+      if (req.session.sino == 'doctor') {
+          var outpatientDepartmentSQL = 'SELECT * FROM patient inner join initial_assessment i using(patient_id) where i.account_id = '+Aid+';';
+          db.query(outpatientDepartmentSQL, function(err, row){
+          if (err) {
+            console.log(err);
+          } else {
+                res.render('doctor/outpatientManagement', {opdInfo:row, admitAvailableBeds:availableBedss});
+            //var patientInfoSQL = 'SELECT * from patient where patient_id = '+rows[0].patient_id+';';
+          }
+        });
+      } else {
+        res.redirect(req.session.sino+'/dashboard');
+      }
+    } else {
+      res.redirect('../login')
+    }
+  });
+
+}
