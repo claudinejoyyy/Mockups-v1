43e569cd023b4a721d3f5023bb186bd276cd11b7
Mamon <cszetsu@gmail.com>
2018-03-07 22:32:02 +0800

    ~app

--- INTEGRATION/H-M-S/app.js
+++ INTEGRATION/H-M-S/app.js
@@ -10,8 +10,14 @@ var session = require('express-session');
 var express = require('express');
 var moment = require('moment');
 var helmet = require('helmet');
-var validator = require('validator');
+var bcrypt = require('bcryptjs');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
 var app = express();
+
+
+
+
 
 app.locals.moment = require('moment');
 app.set('view engine', 'ejs');
@@ -21,25 +27,49 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.static('./public'));
 app.use(session({secret: 'shhhhh', cookie: { maxAge: 3600000 }}));
 
+// Express Validator Middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+//Express Messages Middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
 
 
 //PARA sa DASHBOARD of all MODULES!!!
-var name   = "SELECT name, account_type FROM user_accounts where account_id = ?;";
-var counts = "SELECT (SELECT count(patient_id) er FROM admit WHERE department = 'OPD') as OPD, "
-            +"(SELECT count(patient_id) er FROM admit WHERE department = 'ward') as WARD,"
+var name   = "SELECT name,account_type FROM user_accounts where account_id = ?;";
+var counts = "SELECT (SELECT count(patient_id) From patient p inner join activity_logs a USING(patient_id) where a.type = 'initialAssessment') as OPD, "
+            +"(SELECT count(patient_id) From patient p inner join activity_logs a USING(patient_id) where a.type = 'bed') as WARD,"
             +"(SELECT count(name) pCount FROM patient) as totalRegisteredPatients,"
-            +"(SELECT count(patient_id) admitted FROM admit) as currentlyAdmitted;";
+            +"(SELECT count(patient_id) admitted FROM bed) as currentlyAdmitted;";
 var chart  = "SELECT  (SELECT count(patient_id) from patient where patient_type = 'cadet') as cadet,"
             +"(SELECT count(patient_id) from patient where patient_type = 'military officer') as military_officer,"
             +"(SELECT count(patient_id) from patient where patient_type = 'military dependent') as military_dependent,"
             +"(SELECT count(patient_id) from patient where patient_type = 'civilian') as civilian,"
             +"(SELECT count(patient_id) from patient where patient_type = 'authorized civilian') as authorized_civilian;";
 //COUNT
-var whoOPD               = "SELECT p.name FROM admit INNER JOIN patient p USING(patient_id) WHERE department = 'opd';";
-var whoWARD              = "SELECT p.name FROM admit INNER JOIN patient p USING(patient_id) WHERE department = 'ward';";
-var whoCurrentlyAdmitted = "SELECT p.name, a.patient_id FROM admit a INNER JOIN patient p USING(patient_id);";
+var whoOPD               = "SELECT p.name, a.time From patient p inner join activity_logs a USING(patient_id) where a.type = 'initialAssessment';";
+var whoWARD              = "SELECT p.name, a.time From patient p inner join activity_logs a USING(patient_id) where a.type = 'bed';";
+var whoCurrentlyAdmitted = "SELECT p.name, a.patient_id, a.bed_id FROM bed a INNER JOIN patient p USING(patient_id);";
 // GRAPH
-var currentTime = moment().format('YYYY');
 var monthlyPatientCount  = 'SELECT (SELECT count(logs_id) from activity_logs WHERE time BETWEEN "'+currentTime+'-01-01 00:00:00" and "'+currentTime+'-02-01 00:00:00" and type = "add") as JAN,'
                           +'(SELECT count(logs_id) from activity_logs WHERE time BETWEEN "'+currentTime+'-02-01 00:00:00" and "'+currentTime+'-03-01 00:00:00" and type = "add") as FEB,'
                           +'(SELECT count(logs_id) from activity_logs WHERE time BETWEEN "'+currentTime+'-03-01 00:00:00" and "'+currentTime+'-04-01 00:00:00" and type = "add") as MARCH,'
@@ -58,14 +88,35 @@ var patientList          = "SELECT * from patient;";
 var doctorList           = "SELECT * FROM user_accounts WHERE account_type = 'doctor';";
 var availableBeds        = "SELECT b.bed_id, p.patient_type, p.name, b.status, b.allotment_timestamp from bed b LEFT JOIN patient p USING(patient_id) where b.status = 'Unoccupied';";
 
-var currentTime = moment().format('YYYY-MM-DD HH:mm:ss');
-login (app,db,currentTime);
+var currentTime = moment(new Date()).format('YYYY-MM-DD HH:mm:ss');
+login (app,db,currentTime,bcrypt);
 nurse (app,db,currentTime,name,counts,chart,whoCurrentlyAdmitted,whoOPD,whoWARD,monthlyPatientCount,patientList,doctorList);
 doctor(app,db,currentTime,name,counts,chart,whoCurrentlyAdmitted,whoOPD,whoWARD,monthlyPatientCount,patientList,availableBeds);
-admin (app,db,currentTime,name,counts,chart,whoCurrentlyAdmitted,whoOPD,whoWARD,monthlyPatientCount,patientList,availableBeds);
+admin (app,db,currentTime,name,counts,chart,whoCurrentlyAdmitted,whoOPD,whoWARD,monthlyPatientCount,patientList,availableBeds,bcrypt);
 pharmacist(app,db,currentTime,name,counts,chart,whoCurrentlyAdmitted,whoOPD,whoWARD,monthlyPatientCount,patientList);
 laboratorist(app,db,currentTime,name,counts,chart,whoCurrentlyAdmitted,whoOPD,whoWARD,monthlyPatientCount,patientList);
 
-//port
-app.listen(3001);
+
+
+//FOR AGE INCREMENT
+var CronJob = require('cron').CronJob;
+new CronJob('00 00 * * 1-7', function() {
+    var checkBD = 'SELECT patient_id, birth_date from patient';
+    db.query(checkBD, function(err, rows){
+      var resultDB = JSON.parse(JSON.stringify(rows));
+      for (var i in resultDB) {
+        if (moment(new Date()).format('MM-DD') == moment(resultDB[i].birth_date).format('MM-DD')) {
+          db.query('UPDATE patient SET birth_date="'+ resultDB[i].birth_date+1 +'"where patient_id ='+resultDB[i].patient_id+';', function(err){
+            if (err) {
+              console.log(err);
+            } else {
+              console.log('gumagana');
+            }
+          });
+        }
+      }
+    });
+}, null, true);
+
+app.listen(3000);
 console.log('hello !!');
